4728cdb9faf76a5b3c41400a3383d76cd1dd4a90
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-08-06 12:11:12 +0100

    Reformatted

--- amulet_map_editor/__pyinstaller/hook-numpy.py
+++ amulet_map_editor/__pyinstaller/hook-numpy.py
@@ -12,5 +12,7 @@ if sys.platform == "win32":
         for dll_path in glob.glob(os.path.join(glob.escape(numpy_lib_path), "*.dll"))
     ] + [
         (pyd_path, "numpy")
-        for pyd_path in glob.glob(os.path.join(glob.escape(numpy.__path__[0]), "**", "*.pyd"), recursive=True)
+        for pyd_path in glob.glob(
+            os.path.join(glob.escape(numpy.__path__[0]), "**", "*.pyd"), recursive=True
+        )
     ]

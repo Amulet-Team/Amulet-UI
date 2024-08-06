469f0bd50a331707ea15f60ff9ae0c3e1c8cbdd4
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-08-06 13:02:48 +0100

    Fix dst path

--- amulet_map_editor/__pyinstaller/hook-numpy.py
+++ amulet_map_editor/__pyinstaller/hook-numpy.py
@@ -5,13 +5,14 @@ import sys
 import numpy
 
 if sys.platform == "win32":
-    numpy_lib_path = numpy.__path__[0] + ".libs"
+    numpy_path = numpy.__path__[0]
+    numpy_lib_path = numpy_path + ".libs"
 
     datas = [
         (dll_path, "numpy.libs")
         for dll_path in glob.glob(os.path.join(glob.escape(numpy_lib_path), "*.dll"))
     ] + [
-        (pyd_path, "numpy")
+        (pyd_path, os.path.relpath(os.path.dirname(pyd_path), os.path.dirname(numpy_path)))
         for pyd_path in glob.glob(
             os.path.join(glob.escape(numpy.__path__[0]), "**", "*.pyd"), recursive=True
         )

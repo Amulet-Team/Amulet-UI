2df0aceaf1b8134631fd001a941e23adb5f8eee2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-08-06 12:16:43 +0100

    Reformatted

--- amulet_map_editor/api/wx/ui/select_world.py
+++ amulet_map_editor/api/wx/ui/select_world.py
@@ -58,8 +58,7 @@ if platform == "win32":
     )
     minecraft_world_paths[lang.get("world.bedrock_netease")] = os.path.join(
         os.getenv("APPDATA"),
-        "MinecraftPE_Netease"
-        "minecraftWorlds",
+        "MinecraftPE_Netease" "minecraftWorlds",
     )
 elif platform == "darwin":
     minecraft_world_paths[lang.get("world.java_platform")] = os.path.join(
